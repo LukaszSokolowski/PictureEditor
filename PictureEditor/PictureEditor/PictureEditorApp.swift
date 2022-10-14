f59326aee187fb6e933d09fd5f83603535eb4a56
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2022-10-14 22:43:18 +0200

    Remove core data view

--- PictureEditor/PictureEditor/PictureEditorApp.swift
+++ PictureEditor/PictureEditor/PictureEditorApp.swift
@@ -13,8 +13,6 @@ struct PictureEditorApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
