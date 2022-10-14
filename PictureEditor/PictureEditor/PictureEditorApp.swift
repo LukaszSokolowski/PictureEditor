296975b0e67fb891ab9d61dbff013489197ec023
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2022-10-14 22:37:27 +0200

    InitialCommit

---
+++ PictureEditor/PictureEditor/PictureEditorApp.swift
@@ -0,0 +1,20 @@
+//
+//  PictureEditorApp.swift
+//  PictureEditor
+//
+//  Created by Łukasz Sokołowski on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct PictureEditorApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
