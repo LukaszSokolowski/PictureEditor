110972969f4f002b15f113e9b94cc40db9413a89
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2023-12-18 17:21:24 +0100

    Add PageViewController

---
+++ PictureEditor/PictureEditor/PageViewController/PageViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PageViewController.swift
+//  PictureEditor
+//
+//  Created by Łukasz Sokołowski on 18/12/2023.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
+    }
+    
+    class Coordinator: NSObject {
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+    }
+}
