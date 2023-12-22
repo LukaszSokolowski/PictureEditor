512b5a7cac9b0f9e4fb65f111a374a1528acbcc9
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2023-12-22 22:34:16 +0100

    Pass image to process

--- PictureEditor/PictureEditor/PageView/PageOption/PageOption.swift
+++ PictureEditor/PictureEditor/PageView/PageOption/PageOption.swift
@@ -8,32 +8,45 @@
 import SwiftUI
 
 struct PageOption: View {
+    
+    @State private var processedImage: UIImage
+    
+    init(processedImage: UIImage) {
+        self.processedImage = processedImage
+    }
+    
     var body: some View {
         VStack {
             Button("Equalize histogram") {
-                //processedImage = processImageWith(processMethod: .equalizeHistogram)
+                processedImage = processImageWith(processMethod: .equalizeHistogram)
             }
             .buttonStyle(GradientButton())
             Button("Horizontal reflect") {
-                //processedImage = processImageWith(processMethod: .horizontalReflection)
+                processedImage = processImageWith(processMethod: .horizontalReflection)
             }
             .buttonStyle(GradientButton())
             Button("Vertical reflect") {
-                //processedImage = processImageWith(processMethod: .verticalReflection)
+                processedImage = processImageWith(processMethod: .verticalReflection)
             }
             .buttonStyle(GradientButton())
             Button("Rotate left") {
-                //processedImage = processImageWith(processMethod: .rotateLeft)
+                processedImage = processImageWith(processMethod: .rotateLeft)
             }
             .buttonStyle(GradientButton())
             Button("Rotate right") {
-                //processedImage = processImageWith(processMethod: .rotateRight)
+                processedImage = processImageWith(processMethod: .rotateRight)
             }
             .buttonStyle(GradientButton())
         }
     }
+    
+    func processImageWith(processMethod: ImageProcessMethod) -> UIImage {
+        var imageWrapper = VImageWrapper(uiImage: processedImage)
+        imageWrapper.processImageWith(processMethod: processMethod)
+        return imageWrapper.processedImage ?? UIImage()
+    }
 }
 
 #Preview {
-    PageOption()
+    PageOption(processedImage: .init())
 }
